6d118415e8d2aa85b007365e2901437f6e959abd
AnaMurbl <anamuruzabal@gmail.com>
2025-10-13 18:16:00 +0200

    fix(post): disable Like association until Like is a real Sequelize model

--- src/models/Like.ts
+++ src/models/Like.ts
@@ -1,6 +1,6 @@
-export interface Like {
-  id: number;
-  user_id: number;
-  post_id: number;
-  created_at: string;
-}
+// export interface Like {
+//   id: number;
+//   user_id: number;
+//   post_id: number;
+//   created_at: string;
+// }
